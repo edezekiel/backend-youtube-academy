d4ee0410798c340d944daf500e15161a12985d51
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-28 20:45:16 -0500

    add rest of OAuth Ruby example text

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:show, :index]
-    end 
+      get '/', to 'auth', to: 'auth#check'
+      get '/oauth2callback', to: 'auth#login'
+    end
   end
 end
