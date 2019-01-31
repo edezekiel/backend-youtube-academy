fb05f2430b93544eb0afb2bafba65f8f9b7255e6
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-31 17:04:11 -0500

    add show method to user model and add fetch to profile on frontend

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,15 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
   def index
     users = User.all
     render json: users, status: 200
   end
 
   def show
+    render json: @user, status: 200
+  end
+
+  def set_user
+    @user = User.find_by(googleID: params['id'])
   end
 end
