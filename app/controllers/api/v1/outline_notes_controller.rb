444a17dd362c957ea475ace392b3a72bf2f84669
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-02-06 17:47:11 -0500

    user can finally add outline to a notebook

--- app/controllers/api/v1/outline_notes_controller.rb
+++ app/controllers/api/v1/outline_notes_controller.rb
@@ -1,7 +1,24 @@
 class Api::V1::OutlineNotesController < ApplicationController
+  before_action :set_notebook, only: [:create]
+  before_action :set_outline, only: [:create]
 
   def create
-    byebug
+    @outlineNote = OutlineNote.create(
+        :notebook => @notebook,
+        :outline => @outline)
+    if @outlineNote
+      render json: { outline: @outlineNote.outline, notebook: @outlineNote.notebook }, status: :ok
+    else
+      render json: {errors: 'Unable to render json'},
+        status: :unauthorized
+    end
   end
 
+  def set_notebook
+    @notebook = Notebook.find_by(id: params['notebook'])
+  end
+
+  def set_outline
+    @outline = Outline.find_by(id: params['outline'])
+  end
 end
