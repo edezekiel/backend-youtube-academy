d77b0f4fd3a56ace2f626454344091d83eea6208
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-02-07 15:57:38 -0500

    create outline fetch works on backend

--- app/controllers/api/v1/outlines_controller.rb
+++ app/controllers/api/v1/outlines_controller.rb
@@ -1,21 +1,23 @@
 class Api::V1::OutlinesController < ApplicationController
   before_action :set_outline, only: [:show]
+  before_action :set_user, only: [:create]
 
   def show
     render json: @outline.to_json(:inclue => :notebooks), status: 200
   end
 
   def create
-    @outline = Outline.create(
-        :notes => params[:notes],
-        :videoId => params[:videoId],
-        :videoTitle => params[:videoTitle],
-        :user_id => User.find_by(googleID: params['googleID']).id)
-    if @outline
-      render json: { id: @outline.id, notes: @outline.notes, videoId: @outline.videoId, videoTitle: @outline.videoTitle, user: @outline.user }, status: :ok
-    else
-      render json: {errors: 'Unable to Login In'},
-        status: :unauthorized
+    if @user
+      @outline = @user.outlines.create(
+          :notes => params[:notes],
+          :videoId => params[:videoId],
+          :videoTitle => params[:videoTitle])
+      if @outline
+        render json: { id: @outline.id, notes: @outline.notes, videoId: @outline.videoId, videoTitle: @outline.videoTitle, user: @outline.user }, status: :ok
+      else
+        render json: {errors: 'Unable to render JSON'},
+          status: :unauthorized
+      end
     end
   end
 
@@ -23,4 +25,7 @@ class Api::V1::OutlinesController < ApplicationController
     @outline = Outline.find_by(id: params['id'])
   end
 
+  def set_user
+    @user = User.find_by(email: params['email'])
+  end
 end
